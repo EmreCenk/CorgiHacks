35af1e8426e36f1689e6a9b44d9f59a037ed8839
Emre Cenk <emrecenk9@gmail.com>
2021-03-06 12:31:55 -0500

    sending messages to server is now implemented

---
+++ server/e_app.py
@@ -0,0 +1,36 @@
+
+from flask import Flask, render_template, redirect, url_for, request, Response,session
+
+name_k = "something" #will change for usr
+app = Flask(__name__)
+app.secret_key = "verysecretwewillchangethissoonlol"
+
+
+def log_out():
+
+    a=""
+    session.pop(name_k, None)
+
+
+
+@app.route('/', methods=["POST","GET"])
+def home_page():
+    # if name_k not in session:
+    #     return url_for("set_name")
+    if request.method == 'GET':
+        return render_template("index.html")
+
+    else:
+        message = "message gotten : " + str(request.form["message_sent"])#getting the message
+        #write_message to file
+        #broadcast message
+        print(message)
+        return ('', 204) #returning nothing
+
+
+@app.route('/work')
+def work():
+    print("CALLED PYTHON FROM JAVASCRIPT")
+    return None
+if __name__ == '__main__':
+    app.run(debug=True)
--- server/static/index.js
+++ server/static/index.js
@@ -1,5 +1,31 @@
 
 
+// $(function() {
+//     $('a#somebutton').on('click', function() {
+//         $.getJSON('/work',
+//             function(data) {
+//         //do nothing
+//         });
+//         return false;
+//     });
+//     });
+
+function send_message(){
+
+    var fillform = document.getElementById("message_sending");
+    var message = document.getElementById("message");
+    // console.log(message.value);
+
+    console.log(fillform.textContent);
+    
+
+    fillform.submit();
 
 
 
+}
+
+
+function alpa(){
+    console.log("connected to this file");
+}
